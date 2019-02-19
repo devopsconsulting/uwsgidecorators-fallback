a399cf1509a09db114fd335c30875b6e68da0da1
Lars van de Kerkhof <lars@permanentmarkers.nl>
2019-02-19 10:34:02 +0100

    depend on uwsgidecorators which has been made available on pypi.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ either do nothing or run the function synchronously.
 from setuptools import setup, find_packages
 
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 
 setup(
@@ -50,5 +50,6 @@ setup(
     # specify dependencies
     install_requires=[
         'setuptools',
+        'uwsgidecorators',
     ],
 )
